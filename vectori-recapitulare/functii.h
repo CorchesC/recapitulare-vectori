7b4001ad45d89cc91fdff5da22cd16bff27b8df8
Corches Ciprian <cipriancorches@gmail.com>
2023-02-11 16:29:46 +0200

    problema 1

--- vectori-recapitulare/functii.h
+++ vectori-recapitulare/functii.h
@@ -22,3 +22,23 @@ void afisare(int v[], int dim) {
 	cout << endl;
 
 }
+void stergere(int v[], int& dim, int poz) {
+	for (int i = poz; i < dim; i++) {
+		v[i] = v[i + 1];
+	}
+	dim--;
+}
+void inserare(int v[], int& dim, int poz, int nou) {
+	for (int i = dim; i >= poz; i--) {
+		v[i + 1] = v[i];
+	}
+	dim++;
+	v[poz] = nou;
+}
+void stergereCrescator(int v[], int& dim) {
+	for (int i = 1; i < dim-1; i++) {
+		while (v[i] > v[i + 1]) {
+			stergere(v, dim, i + 1);
+		}
+	}
+}
--- vectori-recapitulare/solutii.h
+++ vectori-recapitulare/solutii.h
@@ -1,3 +1,11 @@
 #pragma once
 #include "functii.h"
 using namespace std;
+
+void sol1() {
+	//(31) Sa se stearga din vectorul A de lungime n, un numar de elemente astfel incat la final sa se obtina un sir strict crescator de elemente. Primul element din vectorul initial nu se va sterge.
+	int v[100], d = 0;
+	citire(v, d);
+	stergereCrescator(v, d);
+	afisare(v, d);
+}
